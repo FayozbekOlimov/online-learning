c5d5277ffb0cc710aa53368bd59119553b370647
FayozbekOlimov <Fayozbek2021dev@gmail.com>
2022-02-19 19:44:14 +0500

    mentors section is ready

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// ===== COURSE DETAILS ===== //
-
 const formatK = a => {
     if (a >= 1000) {
         const b = '' + a / 1000;
@@ -8,10 +6,12 @@ const formatK = a => {
     return a;
 }
 
+// ===== COURSE DETAILS ===== //
 const coursesList = document.querySelector('.courses__list');
 courseData.map(item => {
     const listItem = document.createElement('li');
     listItem.classList.add('courses__item');
+
     listItem.innerHTML = `
         <div class="courses__item-img">
             <img src=${item.image} alt="course-img">
@@ -19,25 +19,53 @@ courseData.map(item => {
         <div class="courses__item-info">
             <div class="courses__item-users">
                 <i class="courses__item-icon fa-regular fa-user"></i>
-                <p class="courses__item-value">${formatK(item.users)}</p>
+                <p class="courses__item-value">${formatK(item.info.users)}</p>
             </div>
             <div class="courses__item-views">
                 <i class="courses__item-icon far fa-eye"></i>
-                <p class="courses__item-value">${formatK(item.views)}</p>
+                <p class="courses__item-value">${formatK(item.info.views)}</p>
             </div>
             <div class="courses__item-rating">
                 <i class="courses__item-icon fa-regular fa-star"></i>
-                <p class="courses__item-value">${item.rating}</p>
+                <p class="courses__item-value">${item.info.rating}</p>
             </div>
             <div class="courses__item-logo">
                 <img src=${item.logo} alt="courses-logo">
             </div>
         </div>
         <div class="courses__item-name">
-            <h3 class="courses__item-title">${item.name}</h3>
+            <h3 class="courses__item-title">${item.title}</h3>
             <h3 class="courses__item-price">$${item.price}</h3>
         </div>
         <p class="courses__item-subtitle">Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.</p>
     `;
     coursesList.appendChild(listItem);
-})
+});
+
+
+// ===== MENTORS DATA ===== //
+const mentorsList = document.querySelector('.mentors__list');
+mentorsData.map(mentor => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('mentors__item');
+
+    listItem.innerHTML = `
+        <div class="mentors__img">
+            <img src=${mentor.image} alt="mentors-img">
+        </div>
+        <h3 class="mentors__name">${mentor.name}</h3>
+        <p class="mentors__job">${mentor.job}</p>
+        <div class="mentors__info">
+            <div class="mentors__info-users">
+                <i class="mentors__info-icon fa-regular fa-user"></i>
+                <p class="mentors__info-value">${formatK(mentor.info.users)}</p>
+            </div>
+            <div class="mentors__info-rating">
+                <i class="mentors__info-icon fa-solid fa-star"></i>
+                <p class="mentors__info-value">${mentor.info.rating}</p>
+            </div>
+            <p class="mentors__info-courses">${mentor.info.course} Courses</p>
+        </div>
+    `;
+    mentorsList.appendChild(listItem);
+});
--- js/store.js
+++ js/store.js
@@ -2,62 +2,107 @@
 const courseData = [
     {
         image: './img/course1.png',
-        users: 5800,
-        views: 69000,
-        rating: 4.5,
-        name: 'Development',
-        price: 150,
+        title: 'Development',
         content: 'Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.',
-        logo: './img/logo/logo1.png'
+        price: 150,
+        logo: './img/logo/logo1.png',
+        info: {
+            users: 5800,
+            views: 69000,
+            rating: 4.5,
+        }
     },
     {
         image: './img/course2.png',
-        users: 5100,
-        views: 54590,
-        rating: 4.6,
-        name: 'Android Developer',
-        price: 130,
+        title: 'Android Developer',
         content: 'Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.',
-        logo: './img/logo/logo2.png'
+        price: 130,
+        logo: './img/logo/logo2.png',
+        info: {
+            users: 5100,
+            views: 54590,
+            rating: 4.6,
+        }
     },
     {
         image: './img/course3.png',
-        users: 4260,
-        views: 21230,
-        rating: 4.2,
-        name: 'JavaScript Course',
-        price: 140,
+        title: 'JavaScript Course',
         content: 'Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.',
-        logo: './img/logo/logo3.png'
+        price: 140,
+        logo: './img/logo/logo3.png',
+        info: {
+            users: 4260,
+            views: 21230,
+            rating: 4.2,
+        }
     },
     {
         image: './img/course4.png',
-        users: 8751,
-        views: 84233,
-        rating: 4.8,
-        name: 'Android Developer',
-        price: 120,
+        title: 'Android Developer',
         content: 'Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.',
-        logo: './img/logo/logo4.png'
+        price: 120,
+        logo: './img/logo/logo4.png',
+        info: {
+            users: 8751,
+            views: 84233,
+            rating: 4.8,
+        }
     },
     {
         image: './img/course5.png',
-        users: 7290,
-        views: 47123,
-        rating: 4.5,
-        name: 'JavaScript Course',
-        price: 120,
+        title: 'JavaScript Course',
         content: 'Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.',
-        logo: './img/logo/logo5.png'
+        price: 120,
+        logo: './img/logo/logo5.png',
+        info: {
+            users: 7290,
+            views: 47123,
+            rating: 4.5,
+        }
     },
     {
         image: './img/course6.png',
-        users: 3359,
-        views: 60120,
-        rating: 4.7,
-        name: 'Basic Marketing',
-        price: 100,
+        title: 'Basic Marketing',
         content: 'Lorem ipsum dolor sit amet, consecte adipiscing elit. Placerat mauris non dictumst in leo. Lorem ipsum dolor sit.',
-        logo: './img/logo/logo6.png'
+        price: 100,
+        logo: './img/logo/logo6.png',
+        info: {
+            users: 3359,
+            views: 60120,
+            rating: 4.7,
+        }
     }
-]
+];
+
+const mentorsData = [
+    {
+        name: 'Mohammod Ali',
+        job: 'CEO, Developer',
+        image: './img/mentor1.png',
+        info: {
+            users: 8236,
+            rating: 4.5,
+            course: 25
+        }
+    },
+    {
+        name: 'Braniya Lara',
+        job: 'Content Writter',
+        image: './img/mentor2.png',
+        info: {
+            users: 7212,
+            rating: 4.8,
+            course: 36
+        }
+    },
+    {
+        name: 'Mohammod Ali',
+        job: 'Content Writter',
+        image: './img/mentor3.png',
+        info: {
+            users: 4537,
+            rating: 4.3,
+            course: 21
+        }
+    }
+];
